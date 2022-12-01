bf07d99c30fdc6860650efef97e4f62d5da1ee1c
soeun <separk314@naver.com>
2022-12-01 09:23:51 +0900

    Feat: 브랜드관련 crud

--- test/lib/network/brand_crud.dart
+++ test/lib/network/brand_crud.dart
@@ -3,39 +3,86 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 class BrandCRUD {
   final currentUser = FirebaseAuth.instance.currentUser;
-  late final brand_db = FirebaseFirestore.instance
+  late final brandDb = FirebaseFirestore.instance
       .collection('user')
       .doc(currentUser!.uid)
       .collection("brand");
 
   // 브랜드 추가
-  void add_brand(String brand_name, String barcode) async {
-    final brand_doc = brand_db.doc(brand_name);
-    brand_doc.set({
+  Future add_brand(String brandName, String barcode) async {
+    final brandDoc = brandDb.doc(brandName);
+    brandDoc.set({
       "brand_barcode": barcode,
     });
-    print("$brand_name 바코드: $barcode, 브랜드 추가 완료");
+    print("$brandName 바코드: $barcode, 브랜드 추가 완료");
   }
 
-  void add_discount_brand(
-      String brand_name, String discount_brand, String discount_barcode) async {
-    final brand_doc = brand_db.doc(brand_name);
-    brand_doc
+  // TODO: 할인 브랜드, 적립 브랜드 없는 브랜드에 추가할 때 exception 만들기
+  // TODO: 없는 컬렉션에 접근햇을 때 어떻게 나오는지
+  // 할인 브랜드 추가
+  Future add_discount_brand(
+      String brandName, String discountBrand, String discountBarcode) async {
+    final brandDoc = brandDb.doc(brandName);
+    brandDoc
         .collection('discount')
-        .doc(discount_brand)
-        .set({"brand_barcode": discount_barcode});
-    print(
-        "$brand_name의 할인 브랜드 - $discount_brand 바코드: $discount_barcode, 추가 완료");
+        .doc(discountBrand)
+        .set({"brand_barcode": discountBarcode});
+    print("$brandName의 할인 브랜드 - $discountBrand 바코드: $discountBarcode, 추가 완료");
   }
 
-  void add_membership_brand(String brand_name, String membership_brand,
-      String membership_barcode) async {
-    final brand_doc = brand_db.doc(brand_name);
-    brand_doc
+  // 적립 브랜드 추가
+  Future add_membership_brand(String brandName, String membershipBrand,
+      String membershipBarcode) async {
+    final brandDoc = brandDb.doc(brandName);
+    brandDoc
         .collection('membership')
-        .doc(membership_brand)
-        .set({"brand_barcode": membership_barcode});
+        .doc(membershipBrand)
+        .set({"brand_barcode": membershipBarcode});
     print(
-        "$brand_name의 멤버쉽 브랜드 - $membership_brand 바코드: $membership_barcode, 추가 완료");
+        "$brandName의 멤버쉽 브랜드 - $membershipBrand 바코드: $membershipBarcode, 추가 완료");
+  }
+
+  // 브랜드 리스트 읽어오기
+  Future get_brand_list() async {
+    final QuerySnapshot snapshot = await brandDb.get();
+    final data = [];
+    for (var doc in snapshot.docs) {
+      var dataElement = doc.data() as Map<String, dynamic>; // 브랜드 바코드
+      dataElement["brand_name"] = doc.id; // 브랜드 이름
+      // 할인 브랜드 리스트
+      var brand_list = await get_discount_brand_list(doc.id);
+      dataElement["discount_list"] = brand_list;
+      // 적립 브랜드 리스트
+      var membership_list = await get_membership_brand_list(doc.id);
+      dataElement["membership_list"] = membership_list;
+      data.add(dataElement);
+    }
+    return data;
+  }
+
+  // 할인 브랜드 읽어오기
+  Future get_discount_brand_list(String brand) async {
+    final QuerySnapshot snapshot =
+        await brandDb.doc(brand).collection("discount").get();
+    final data = [];
+    for (var doc in snapshot.docs) {
+      var dataElement = doc.data() as Map<String, dynamic>;
+      dataElement["discount_brand_name"] = doc.id;
+      data.add(dataElement);
+    }
+    return data;
+  }
+
+  // 적립 브랜드 읽어오기
+  Future get_membership_brand_list(String brand) async {
+    final QuerySnapshot snapshot =
+        await brandDb.doc(brand).collection("membership").get();
+    final data = [];
+    for (var doc in snapshot.docs) {
+      var dataElement = doc.data() as Map<String, dynamic>;
+      dataElement["membership_brand_name"] = doc.id;
+      data.add(dataElement);
+    }
+    return data;
   }
 }
